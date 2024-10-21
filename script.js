e9960a02d7243a26140274a3fdf3e8661ea84545
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-10-21 20:45:52 +0530

    JS Functions practice

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 
-//Anonymous function
+// Anonymous function (nameless function)
 var movie=function(movieName){
     return movieName
 }
 console.log(movie("Vikram"));
 
-//Arrow function 
+// Arrow function 
 var greet=(user)=>{
     console.log("Welcome",user)
 }
+console.log(user("Hitesh"))
 
-greet("Vicky")
+// Immediately Invoked function Expression 
+(function userLogin(userName){
+    console.log(`Hello ${userName}`);
+}("Ajay"))
+
+// Higher Order Functions 
+
+
+function naveen(task){
+     // Performing some operations
+    console.log("Hey Naveen")
+     // Calling the callback function
+    task()
+}
+ // Callback function to be passed to the higher order function
+function task(){
+     console.log("Cook Dinner")
+}
+
+// Calling the higher order function with the callback function as argument
+naveen(task);
+
