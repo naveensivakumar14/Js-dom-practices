8f7292e330a0bb3d2ef5cef6be8d58082f0e3329
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-10-28 21:01:18 +0530

    JS template literals

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ var fruits=["Apple","Banana","Grapes"] //array
 var basket={fruitName:"Grapes", price:150, kg:1} //object
 
 
+//Template Literals
+var Stu_name="Vignesh"
+var Stu_age=22
+
+console.log(`Hi, I am ${Stu_name}, my age is ${Stu_age}`)
