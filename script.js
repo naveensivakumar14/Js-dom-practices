b8e9e5005784e865df1d3daad20ae26a9bb6e8a1
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-10-29 22:34:02 +0530

    JS Practice map function

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ var basket={fruitName:"Grapes", price:150, kg:1} //object
 
 
 //Template Literals
-var Stu_name="Vignesh"
-var Stu_age=22
+// var Stu_name="Vignesh"
+// var Stu_age=22
 
-console.log(`Hi, I am ${Stu_name}, my age is ${Stu_age}`)
+// console.log(`Hi, I am ${Stu_name}, my age is ${Stu_age}`)
+
+// for(let i=0;i<length.fruits;i++){
+//     console.log(fruits[i])
+// }
+
+let fruitsBasket= ["Apple", "Orange", "Grapes", "Pineapple", "Watermelon", "Papaya", "Banana", "Strawberry", "Mango"];
+
+fruitsBasket.map((fruit) => {
+    console.log(fruit);
+});
+
+
