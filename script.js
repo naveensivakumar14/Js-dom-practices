250019d257cf39e136a995845c56af460508b716
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-11-01 21:45:16 +0530

    JS Practice November 1

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-var num=25              //Number
-var name="Gokul"        //String
-var percent=89.35       //Number  
-var eliglible=true      //Boolean
-var data=undefined      //undefined
-var count=null          //object
-var fruits=["Apple","Banana","Grapes"] //array
-var basket={fruitName:"Grapes", price:150, kg:1} //object
-
-
-//Template Literals
-// var Stu_name="Vignesh"
-// var Stu_age=22
-
-// console.log(`Hi, I am ${Stu_name}, my age is ${Stu_age}`)
-
-// for(let i=0;i<length.fruits;i++){
-//     console.log(fruits[i])
-// }
 
 let fruitsBasket= ["Apple", "Orange", "Grapes", "Pineapple", "Watermelon", "Papaya", "Banana", "Strawberry", "Mango"];
 
-fruitsBasket.map((fruit) => {
-    console.log(fruit);
+fruitsBasket.map((fruit,index) => {
+    console.log(index+1,fruit);
 });
 
-
+fruitsBasket.forEach(fruitName => {
+    console.log(fruitName)
+});
