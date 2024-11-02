708a43f266b6089ac0d49990bae021888cd8b8ef
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-11-02 17:10:13 +0530

    Object Destructuring ES6

--- Javascript/object.js
+++ Javascript/object.js
@@ -5,3 +5,15 @@ for (let day in dinnerMenu){
     console.log("Day: ",day,"   Food: ",dinnerMenu[day])
 }
 console.log(dinnerMenu)
+
+//destructuring objects
+var {Mon,Tues,Wednes,Thurs,Fri}=dinnerMenu
+console.log(Mon,Fri)
+
+//destructuring obejct
+var person={name:"Sanjay",age:22,name:"Aswin",age:26}
+var {name,name}=person
+var {age,age}=person
+
+console.log(name)
+console.log(age)
