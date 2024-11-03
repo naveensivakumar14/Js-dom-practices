7f9cd33e3a5adf510afe0f22cc7f2e927280a1ec
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-11-03 21:40:51 +0530

    ES6

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -22,9 +22,55 @@ var show= movie("Amaran")
 console.log(show)
 console.log(movie("Brother"))
 
-//no arguments with return values
+//no arguments with return valuess
 function movie(){
     return "Blody Begger"
 }
 
 console.log(movie())
+
+
+
+
+//ES6 Functions
+
+
+//Named functions
+console.log("Named function")
+function add(a,b){
+    console.log(a+b)
+}
+add(2,4)
+
+//Anonymous functions
+console.log("\nAnonymous function")
+const vote = function(age) {
+    console.log(age >= 18 ? "Eligible to Vote" : "Underage");
+};
+vote(20)
+vote(12)
+
+//Arrow functions 
+console.log("\nArrow function")
+var greeting=(guest)=>{
+    console.log("Welcome ",guest);
+}
+greeting("Arjun")
+
+//Callback function
+console.log("Callback function")
+function partner(){
+    console.log("Swathi")
+}
+function greet(partener){
+    console.log("My partner is");
+    partner()
+    
+}
+greet(partner)
+
+
+//IIFE
+(function() {
+    console.log("This function runs immediately!");
+  })();
--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 
-let fruitsBasket= ["Apple", "Orange", "Grapes", "Pineapple", "Watermelon", "Papaya", "Banana", "Strawberry", "Mango"];
-fruitsBasket.map((fruit,index) => {
-    console.log(index+1,fruit);
-});
-fruitsBasket.forEach(fruitName => {
-    console.log(fruitName)
-});
+// let fruitsBasket= ["Apple", "Orange", "Grapes", "Pineapple", "Watermelon", "Papaya", "Banana", "Strawberry", "Mango"];
+// fruitsBasket.map((fruit,index) => {
+//     console.log(index+1,fruit);
+// });
+// fruitsBasket.forEach(fruitName => {
+//     console.log(fruitName)
+// });
 
+
+//Named functions
+console.log("Named function")
+function add(a,b){
+    console.log(a+b)
+}
+add(2,4)
+
+//Anonymous functions
+console.log("\nAnonymous function")
+const vote = function(age) {
+    console.log(age >= 18 ? "Eligible to Vote" : "Underage");
+};
+vote(20)
+vote(12)
+
+//Arrow functions 
+console.log("\nArrow function")
+var greeting=(guest)=>{
+    console.log("Welcome ",guest);
+}
+greeting("Arjun")
+
+//Callback function
+console.log("Callback function")
+function partner(){
+    console.log("Swathi")
+}
+function greet(partener){
+    console.log("My partner is");
+    partner()
+    
+}
+greet(partner)
+
+
+//IIFE
+(function() {
+    console.log("This function runs immediately!");
+  })();
 
