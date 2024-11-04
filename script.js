9e6fdee93c14c06e80feffee4b6058b0dc8ab85b
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-11-04 12:05:48 +0530

    Map Filter ForEach practice

--- script.js
+++ script.js
@@ -9,3 +9,41 @@
 
 
 
+//25.1 - JS Array methods 
+
+
+//ForEach method
+let fruits=["Orange","Apple","Grapes"]
+
+fruits.forEach(function(fruit,index){
+    
+    if(fruit=="Apple"){
+        console.log(index)
+    }
+
+    console.log(fruit)
+    
+})
+
+
+//Filter function
+let numbers=[25,17,34,62,59,90,1]
+
+var above50=numbers.filter(function(value){
+    if(value>50){
+        return true
+    }
+    else{
+        return false
+    }
+})
+
+console.log(above50)
+
+
+//Map function
+var biscuits=["GoodDay","Orea","MarieGold","Hide & Seek","Unibic"]
+
+biscuits.map(function(item){
+    console.log(item)
+})
