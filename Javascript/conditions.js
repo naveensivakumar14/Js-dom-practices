f487ac76bcf561d1eb2cc23bcaab67fd1cfad91f
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-11-09 20:05:20 +0530

    conditions

--- Javascript/conditions.js
+++ Javascript/conditions.js
@@ -1,24 +1,46 @@
-let day = 3;
-let dayName;
+// let day = 3;
+// let dayName;
 
-switch (day) {
-    case 1:
-        dayName = "Monday";
-        break;
-    case 2:
-        dayName = "Tuesday";
-        break;
-    case 3:
-        dayName = "Wednesday";
-        break;
-    case 4:
-        dayName = "Thursday";
-        break;
-    case 5:
-        dayName = "Friday";
-        break;
-    default:
-        dayName = "Invalid day";
+// switch (day) {
+//     case 1:
+//         dayName = "Monday";
+//         break;
+//     case 2:
+//         dayName = "Tuesday";
+//         break;
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     default:
+//         dayName = "Invalid day";
+// }
+
+// console.log(dayName); 
+
+//IF
+// var cold=false
+
+// if(cold==false){
+//     console.log("Eat Ice cream")
+// }
+
+
+//else if
+
+var temperature = 26; // You can change this value to test different conditions
+
+if (temperature > 30) {
+    console.log("It's really hot outside!");
+} else if (temperature > 20) {
+    console.log("It's warm outside. Perfect for an ice cream!");
+} else if (temperature > 10) {
+    console.log("It's a bit chilly. Wear a jacket!");
+} else {
+    console.log("It's cold. Stay warm indoors!");
 }
-
-console.log(dayName); // Output: Wednesday
