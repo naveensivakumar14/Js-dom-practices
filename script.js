70761cbbf327c7aa66546c31eb6813d777efdf65
naveensivakumar14 <ersnaveenkumar@gmail.com>
2026-01-01 20:45:26 +0530

    Jan 1 JS Practice 2026

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
 // Initialize an empty basket
 const basket=[];
 
-// Function to add items to the basket
+// Function to add items to the basket (function definition)
 function addToBasket(item){
     basket.push(item);
     console.log(`${item} added to basket.$`);
 }
 
-// Adding items to the basket
+// Adding items to the basket (function calls)
 addToBasket (["Appple","wheat bread","milk","eggs","soya chunks"]);
 
 // View basket items
 function viewBasket(){
     console.log("Items in basket:");
     basket.forEach((item, index) => {
+        
         console.log(`${index + 1}. ${item}`);
     }); 
 }
