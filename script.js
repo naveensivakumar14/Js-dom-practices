c3c8fdadbcaaf03e47b23d5b3c5f5091cafacd59
naveensivakumar14 <ersnaveenkumar@gmail.com>
2026-01-17 22:34:53 +0530

    JS operator practice

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-let amount=[200,150,300,400,250];
+//Operators in JavaScript
+
+let a=4
+let b=2;
+
+console.log("a =", a);
+console.log("b =", b);
+
+//arithmetic operators
+console.log("a + b =", a + b); // addition
+console.log("a - b =", a - b); // subtraction
+console.log("a * b =", a * b); // multiplication
+console.log("a / b =", a / b); // division
+console.log("a % b =", a % b); // modulus
+console.log("a ** b =", a ** b); // exponentiation //4^2 = 16
+
+//assignment operators
+let c = 5;
+console.log("Initial c =", c);
+c += 10; // c = c + 10
+console.log("After c += 10, c =", c);
+c -= 3; // c = c - 3
+console.log("After c -= 3, c =", c);
+c *= 2; // c = c * 2
+console.log("After c *= 2, c =", c);
+c /= 4; // c = c / 4
+console.log("After c /= 4, c =", c);
+
+c %= 3; // c = c % 3
+console.log("After c %= 3, c =", c); 
+
+c=10; // reset c to 4
+c **= 2; // c = c ** 2
+console.log("After c **= 2, c =", c); 
 
 
-for(let i=0;i<amount.length;i++){
-    if(amount[i]>200){
-        console.log("Product price is high:",amount[i]);
-
-    }else{
-        console.log("Product price is low:",amount[i]);
-    }
-
-}
-
+//comparison operators
