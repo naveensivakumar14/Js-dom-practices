aa11b30d3e9b43f7a18a56abdcc7692ecc3e1192
naveensivakumar14 <ersnaveenkumar@gmail.com>
2026-01-18 22:07:21 +0530

    JS Objects Practice

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-//Operators in JavaScript
-
-let a=4
-let b=2;
-
-console.log("a =", a);
-console.log("b =", b);
-
-//arithmetic operators
-console.log("a + b =", a + b); // addition
-console.log("a - b =", a - b); // subtraction
-console.log("a * b =", a * b); // multiplication
-console.log("a / b =", a / b); // division
-console.log("a % b =", a % b); // modulus
-console.log("a ** b =", a ** b); // exponentiation //4^2 = 16
-
-//assignment operators
-let c = 5;
-console.log("Initial c =", c);
-c += 10; // c = c + 10
-console.log("After c += 10, c =", c);
-c -= 3; // c = c - 3
-console.log("After c -= 3, c =", c);
-c *= 2; // c = c * 2
-console.log("After c *= 2, c =", c);
-c /= 4; // c = c / 4
-console.log("After c /= 4, c =", c);
-
-c %= 3; // c = c % 3
-console.log("After c %= 3, c =", c); 
-
-c=10; // reset c to 4
-c **= 2; // c = c ** 2
-console.log("After c **= 2, c =", c); 
 
 
-//comparison operators
+
+//Destrcturing
+
+let user = {
+    name: "Steven",
+    age: 30,
+    city: "Boston",
+    work: "Engineer"
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.city);
+console.log(user.work);
+
+let {name, age, city, work} = user; //destructuring
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(work);
